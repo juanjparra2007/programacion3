1812ac58df6442de4f5ab5ffd1d5534a901312ad
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-13 00:23:40 +0000

    Declaración de matrices segunda forma

---
+++ Semana3/MatricesB.java
@@ -0,0 +1,25 @@
+public class MatricesB {
+    public static void main(String[] args) {
+
+    //Declaracion del arreglo forma 1
+    int[] a = {3, 10, 5, 9, 11, 22, 1, 14, 25, 7, 8, 19};
+    
+    //Recorrer y mostrar el arreglo
+
+    for(int i = 0; i < a.length; i++){
+        System.out.println("a[" + i + "] = " + a[i]);
+    }
+    //Declaracion del arreglo forma 2
+
+    int[] b = new int [12];
+
+    int max = 100;
+    int min = 1;
+
+    for (int i = 0; i < b.length; i++){
+        b[i] = (int)(Math.random() * (max - min + 1) + min);
+        System.out.println("b[" + i + "] = " + b[i]);
+    }
+    
+    }
+} 
