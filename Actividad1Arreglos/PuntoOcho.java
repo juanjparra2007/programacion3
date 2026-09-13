5e54d387bc92a435e6a28a630aaca8f21af9e9b4
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-13 01:49:43 +0000

    Se agrega un for para buscar el mayor y menor

--- Actividad1Arreglos/PuntoOcho.java
+++ Actividad1Arreglos/PuntoOcho.java
@@ -9,5 +9,19 @@ public class PuntoOcho {
         for(int i = 0; i < a.length; i++) {
             a[i] = (int) (Math.random() * (max - min + 1) + min);
         }
+
+        System.out.println("---------------------------------");
+
+        int mayor = a[0];
+        int menor = a[0];
+
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] > mayor) {
+                mayor = a[i];
+            }
+            if(a[i] < menor) {
+                menor = a[i];
+            }
+        }
     }
 }
