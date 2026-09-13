5ffdb9fdd98da1ced28baf42c359e9318fb71cb7
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-13 00:57:11 +0000

    Correción de errores

--- Actividad#1Arreglos/PuntoSiete.java
+++ Actividad#1Arreglos/PuntoSiete.java
@@ -3,9 +3,13 @@ public class PuntoSiete {
 
         int[] a = {122, 45, 8, 88, 10, 32, 38, 1, 55, 99};
 
-        for(int i = 0; i <a.length; i++) {
-            system.out.println("a[" + i + "] = " + a[i]);
+        System.out.println("Arreglo original");
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
         }
+
+        System.out.println("-----------------------");
 
         int[] imp = new int[10];
         int[] pa = new int[10];
@@ -24,12 +28,19 @@ public class PuntoSiete {
             }
         }
 
+        System.out.println("-----------------------");
+
         System.out.println("Pares: ");
         for(int i = 0; i < p; i++) {
             System.out.println("pa[" + i +"] = " + pa[i]);
         }
 
-        System.out.println
+        System.out.println("-----------------------");
+
+        System.out.println("Impares: ");
+        for(int i = 0; i < im; i++) {
+            System.out.println("imp[" + i +"] = " + imp[i]);
+        }
 
 
     }
