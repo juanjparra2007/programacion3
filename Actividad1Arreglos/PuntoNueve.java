116007502d889568d4f4ac3e01b666240ec4d5d4
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 00:30:36 +0000

    pedir e ingresar los datos del arreglo usando un for

--- Actividad1Arreglos/PuntoNueve.java
+++ Actividad1Arreglos/PuntoNueve.java
@@ -8,5 +8,12 @@ public class PuntoNueve {
         int tam = sc.nextInt();
 
         int[] a = new int[tam];
+
+        System.out.println("Ingrese los "+tam+"elmentos del arreglo: ");
+        for(int i = 0; i < tam; i++) {
+            System.out.println("Elemento [" +i+"]:");
+            a[i] = sc.nextInt();
+        }
+
     }
 }
