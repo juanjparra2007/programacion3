bce22c659903e0e08ac1765e6aeaf4fcf9ff64ac
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 05:36:22 +0000

    Correción de errores, ajustamiento del código y impresión del resultado

--- Actividad1Arreglos/PuntoDiez.java
+++ Actividad1Arreglos/PuntoDiez.java
@@ -3,16 +3,26 @@ public class PuntoDiez {
         int[] a = {10, 20, 3, 7, 9, 8, 2};
         int n = a.length;
 
-        int tamaB = (n +1)/2;
+        System.out.println("---Arreglo original---");
+        for(int i = 0; i < a.length; i++){
+            System.out.println("a[" + i + "]= " + a[i]);
+        }
+
+        int tamaB = (n +1) / 2;
         int[] b = new int[tamaB];
 
-        for(i = 0; i < tamaB; i++) {
-            int opu = n -1 -i;
+        for(int i = 0; i < tamaB; i++) {
+            int opu = n - 1 - i;
             if(i == opu) {
-                b[i] = a[1];
+                b[i] = a[i];
             } else {
-                b[i] = a[i] +a[opu];
+                b[i] = a[i] + a[opu];
             }
         }
+        System.out.println("---Arreglo resultante b---");
+        for(int i = 0; i < b.length; i++){
+            System.out.println("b[" + i + "]= " + b[i]);
+        }
+
     }
 }
