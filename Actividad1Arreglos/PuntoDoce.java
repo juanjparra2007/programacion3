a5d108d27d918c81b02a9e888242906f4ccf9128
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 20:23:30 +0000

    Importación el scanner, declaración tamaño del arreglo y declaración de arreglo

---
+++ Actividad1Arreglos/PuntoDoce.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PuntoDoce {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrse el tamño del arreglo: ");
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+
+
+    }
+}
