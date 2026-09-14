0c507e02bc6f27345a8f77f8d54f11fe61c7c07a
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 00:58:21 +0000

    Correción de errores y acomodamiento del código

--- Actividad1Arreglos/PuntoNueve.java
+++ Actividad1Arreglos/PuntoNueve.java
@@ -4,18 +4,18 @@ public class PuntoNueve {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrse el tamaño del arreglo: ");
+        System.out.println("Ingrese el tamaño del arreglo: ");
         int tam = sc.nextInt();
 
         int[] a = new int[tam];
 
-        System.out.println("Ingrese los "+tam+"elmentos del arreglo: ");
+        System.out.println("Ingrese los "+ tam + " elementos del arreglo: ");
         for(int i = 0; i < tam; i++) {
-            System.out.println("Elemento [" +i+"]:");
+            System.out.println("Elemento [" + i + "]:");
             a[i] = sc.nextInt();
         }
 
-        System.out.println("Ingrse el número que se desea buscar:");
+        System.out.println("Ingrese el número que se desea buscar: ");
         int numB = sc.nextInt();
 
         int conta = 0;
@@ -25,20 +25,8 @@ public class PuntoNueve {
             }
         }
 
-        System.out.println("El número " + numB + " se repite "+ conta + " vecces en el arreglo.");
+        System.out.println("El número " + numB + " se repite "+ conta + " veces en el arreglo.");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        sc.close();
     }
 }
