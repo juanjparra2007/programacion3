21c60c9a0f70b2ee8809882c6c137cd59eb3d220
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 00:19:47 +0000

    Se agrega el scanner para pedir el tamaño del arreglo

---
+++ Actividad1Arreglos/PuntoNueve.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PuntoNueve {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrse el tamaño del arreglo: ");
+        int tam = sc.nextInt();
+
+        int[] a = new int[tam];
+    }
+}
