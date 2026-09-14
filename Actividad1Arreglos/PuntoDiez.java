3b2f3ea13398d6b9247851e234fbdd088d49d9f1
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 04:12:08 +0000

    Se declara el arreglo a y después se calcula el tamaño del nuevo arreglo

---
+++ Actividad1Arreglos/PuntoDiez.java
@@ -0,0 +1,11 @@
+public class PuntoDiez {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 3, 7, 9, 8, 2};
+        int n = a.length;
+
+        int tamaB = (n +1)/2;
+        int[] b = new int[tamaB];
+
+
+    }
+}
