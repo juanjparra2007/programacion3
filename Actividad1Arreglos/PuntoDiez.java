ffd4ca14d1fef42fb23ca38aa245f56320ef9327
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 05:10:45 +0000

    Se usa un for para calcular el arreglo b y su suma con los opuestos

--- Actividad1Arreglos/PuntoDiez.java
+++ Actividad1Arreglos/PuntoDiez.java
@@ -6,6 +6,13 @@ public class PuntoDiez {
         int tamaB = (n +1)/2;
         int[] b = new int[tamaB];
 
-
+        for(i = 0; i < tamaB; i++) {
+            int opu = n -1 -i;
+            if(i == opu) {
+                b[i] = a[1];
+            } else {
+                b[i] = a[i] +a[opu];
+            }
+        }
     }
 }
