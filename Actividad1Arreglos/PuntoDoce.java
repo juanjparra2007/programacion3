4f9440b23eaa552c206453004a7a9595fb0de64d
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-14 21:06:28 +0000

    Correción de código e impresión

--- Actividad1Arreglos/PuntoDoce.java
+++ Actividad1Arreglos/PuntoDoce.java
@@ -4,19 +4,19 @@ public class PuntoDoce {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingrse el tamño del arreglo: ");
+        System.out.print("Ingrese el tamaño del arreglo: ");
         int n = sc.nextInt();
 
         int[] a = new int[n];
 
 
-        System.out.println("Ingrse los "+ n + " elmentos del arrglo:");
+        System.out.println("Ingrese los "+ n + " elementos del arreglo:");
         for(int i = 0; i < n; i++) {
-            System.out.print("a[" +i+"]= " );
+            System.out.print("a[" + i +"]= " );
             a[i] = sc.nextInt();
         }
 
-        System.out.print("Ingrse el valor de X a buscar; ");
+        System.out.print("Ingrese el valor de X a buscar: ");
         int x = sc.nextInt();
 
         int tamaB = 0;
@@ -26,25 +26,25 @@ public class PuntoDoce {
             }
         }
 
-        if(tamaB == 0){
-            System.out.println("El número" +x+ "no se encuentra en el arreglo");
+        if(tamaB == 0) {
+            System.out.println("El número" + x + "no se encuentra en el arreglo");
         } else {
-            int[] b = new int[tamaB];
             
+            int[] b = new int[tamaB];
             int posB = 0;
 
             for(int i = 0; i < n; i++) {
                 if(a[i] == x) {
-                    b[posB] = i+ 1;
+                    b[posB] = i + 1;
                     posB++;
                 }
             }
+
+            System.out.println("\n--- Arreglo b (Posiciones de " + x + ") ---");
+            for(int i = 0; i < b.length; i++) {
+                System.out.println("b[" + i + "]= " + b[i]);
+            }
         }
-
-
-
-
-
-
+        sc.close();
     }
 }
