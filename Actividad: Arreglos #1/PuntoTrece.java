cadea0e715304bea6645276348ab3368c62eae5f
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-15 00:08:01 +0000

    Pedir el tamaño del arreglo

---
+++ Actividad: Arreglos #1/PuntoTrece.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+
+public class PuntoTrece {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese el rtamño del arreglo; ");
+        int n = sc.nexInt();
+
+        int[] a = new int[n];
+
+    }
+}
