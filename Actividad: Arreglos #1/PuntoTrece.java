270f70673b8a61b96c7582adec013da2ea7ab3f3
juanjparra2007 <juanjparra@estudiante.uniajc.edu.co>
2026-09-15 01:02:05 +0000

    Llenar el arreglo (a) y calcular la suma

--- Actividad: Arreglos #1/PuntoTrece.java
+++ Actividad: Arreglos #1/PuntoTrece.java
@@ -9,13 +9,18 @@ public class PuntoTrece {
 
         int[] a = new int[n];
 
-        int[] b = new int[n];
-        int[] c = new int[n];
+        int[] b = new int[a.length];
+        int[] c = new int[a.length];
 
 
         int suma = 0;
         int may = 0;
         int men = 0,
+
+        for(int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 100) + 1;
+            suma += a[i];
+        }
 
 
 
